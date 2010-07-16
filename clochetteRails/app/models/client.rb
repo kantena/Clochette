92c3002e5e56fd18ccdecb6b6d747663626b04de
damien <damien@kantena.com>
2010-07-16 17:42:02 +0200

    merges

--- clochetteRails/app/models/client.rb
+++ clochetteRails/app/models/client.rb
@@ -1,6 +1,8 @@
+
 class Client < ActiveRecord::Base
   validates_presence_of :nom
   validates_uniqueness_of :nom
   validates_numericality_of :jours_a_facturer_mois_courant
   validates_inclusion_of :jours_a_facturer_mois_courant, :in => (0..31)
+
 end
