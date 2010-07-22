b1244a2ac3bf5456bf4609b902d80833a351e9ec
nfe <nfe@kantena.com>
2010-07-22 15:05:05 +0200

    ajout de nouveaux comptes rendu d'activités

--- app/controllers/activity_notes_controller.rb
+++ app/controllers/activity_notes_controller.rb
@@ -1,5 +1,6 @@
 class ActivityNotesController < ApplicationController
   def index
+    # TODO replace 1 by developper id
     @activity_notes = ActivityNote.find_all_by_developper_id 1
   end
 
@@ -17,6 +18,19 @@ class ActivityNotesController < ApplicationController
   def destroy
     activity_note = ActivityNote.find params[:id]
     activity_note.destroy
+    redirect_to :action => "index"
+  end
+
+  def new
+  end
+
+  def create
+    activity_note = ActivityNote.new
+    # TODO replace 1 by developper id
+    activity_note.developper_id = 1
+    activity_note.working_days = params[:activity_note][:working_days]
+    activity_note.customer_id = params[:activity_note][:customer_id]
+    activity_note.save
     redirect_to :action => "index"
   end
 end
--- spec/controllers/activities_notes_controller_spec.rb
+++ spec/controllers/activities_notes_controller_spec.rb
@@ -22,9 +22,14 @@ describe ActivityNotesController do
       equipier = Factory(:kantenien, :name =>'nicolas')
       activity_note = Factory(:activity_note, :customer => company, :developper => equipier, :working_days => 10 )
       get 'destroy', :id => activity_note.id
-      response.should redirect_to :action => "index"
+      response.should redirect_to( :action => "index")
       note = ActivityNote.find :all
       assert_equal 0, note.size
+    end
+
+    it "affiche la page d'ajout de nouveau compte rendu" do
+      get 'new'
+      response.should be_success
     end
   end
 
@@ -47,8 +52,17 @@ describe ActivityNotesController do
       equipier = Factory(:kantenien, :name =>'nicolas')
       note =  Factory(:activity_note, :customer => company, :developper => equipier, :working_days => 10 )
       put 'update', :id => note.id, :activity_note => {:working_days => 1}
-      response.should redirect_to :action => "index"
+      response.should redirect_to( :action => "index")
       note_upated = ActivityNote.find note.id
+      assert_equal 1, note_upated.working_days
+    end
+    
+    it "cree un nouveau compte rendu d'activité" do
+      company = Factory(:customer, :name => 'Vinci')
+      equipier = Factory(:kantenien, :name =>'nicolas')
+      put 'create',:activity_note => {:working_days => 1, :customer_id => company.id}
+      response.should redirect_to( :action => "index")
+      note_upated = ActivityNote.find :first
       assert_equal 1, note_upated.working_days
     end
   end
